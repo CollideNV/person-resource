c52fcf995fb7eea7091d8d3358f1f4de9f23b58a
Berten De Schutter <berten@gmail.com>
2024-10-14 15:37:58 +0200

    Make Unit Test better

--- src/test/java/be/collide/resource/PersonResourceTest.java
+++ src/test/java/be/collide/resource/PersonResourceTest.java
@@ -1,8 +1,10 @@
 package be.collide.resource;
 
 import io.quarkus.test.junit.QuarkusTest;
+
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 import static io.restassured.RestAssured.given;
@@ -20,7 +22,11 @@ class PersonResourceTest {
     @Test
     void testCreateAndFindById() {
 
-        var createPerson = PersonResource.CreatePersonDto.builder().build();
+        var createPerson = PersonResource.CreatePersonDto.builder()
+                .name("Person Last Name")
+                .firstName("Person First name")
+                .birthDate(LocalDate.of(2000,4,14))
+                .build();
 
         String url = given()
                 .contentType("application/json")
@@ -30,13 +36,15 @@ class PersonResourceTest {
                 .statusCode(201)
                 .extract().headers().get("Location").getValue();
 
-
         PersonResource.PersonDTO personDTO = given()
                 .when().get(url)
                 .then()
                 .statusCode(200).extract().as(PersonResource.PersonDTO.class);
 
         assertThat(personDTO.getId()).isNotNull();
+        assertThat(personDTO.getName()).isEqualTo("Person Last Name");
+        assertThat(personDTO.getFirstName()).isEqualTo("Person First name");
+        assertThat(personDTO.getBirthDate()).isEqualTo(LocalDate.of(2000,4,14));
     }
 
 }
